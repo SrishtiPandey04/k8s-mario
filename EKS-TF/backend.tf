3a09afc9f1c859990d96ebdec63b171c0d73d4db
SrishtiPandey04 <161590808+SrishtiPandey04@users.noreply.github.com>
2024-07-26 10:36:31 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
+    bucket = "srishti-mario-bucket" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
